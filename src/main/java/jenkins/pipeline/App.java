9bd3a596b64c4918babe9a3ea1cc5c64438937f3
varshneylavina <lavinavarshney1704@gmail.com>
2024-12-03 13:55:52 +0530

    Jenkin files changes

--- src/main/java/jenkins/pipeline/App.java
+++ src/main/java/jenkins/pipeline/App.java
@@ -4,10 +4,9 @@ package jenkins.pipeline;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Thank you from Jenkins");
     }
 }
